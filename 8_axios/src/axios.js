839b81eb733a82504a0ddacccc8e4134af347c3a
quwensong <1607186824@qq.com>
2023-06-27 16:54:26 +0800

    ✨ feat:

--- 8_axios/src/axios.js
+++ 8_axios/src/axios.js
@@ -1,1 +1,12 @@
-class Axios {}
+class Axios {
+  // 用来存储配置信息。
+  config = {};
+  constructor(initConfig) {
+    // 实例化时接收一个配置信息，并保存到config属性中。
+    this.config = initConfig;
+  }
+  // 该类有一个 request 方法，它可以用来发送请求
+  request(config) {}
+}
+
+console.log(Axios);
